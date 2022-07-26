4f394b7f437637b8b71bbbd231e713ab4ae0fb8c
rantavishav <rantavishav49@gmail.com>
2022-07-27 00:51:02 +0530

    Made App in arrow function for consistency

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { BG_IMG_JPG } from './assests/images';
 import { Picture } from './components';
 import ToDoList from './pages/toDoList';
 
-function App() {
+const App = () => {
   return (
     <div className="main-container">
       <Picture img={BG_IMG_JPG} className="todo-background" />
       <ToDoList />
     </div>
   );
-}
+};
 
 export default App;
